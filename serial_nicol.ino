e5ff6bce44b6aee568927af4f57b1df76b62c2f8
nicolituh <78593804+nicolituh@users.noreply.github.com>
2021-02-12 11:22:51 +0100

    Add files via upload

---
+++ serial_nicol.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello My name is Ituh Nicol, 16CJ020731");
+  delay(3000);
+}
